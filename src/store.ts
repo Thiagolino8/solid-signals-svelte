201a97ca5ee2b15ca6d2ba734eb533caf6021168
Thiago <thiagolinog8@gmail.com>
2023-03-07 18:11:27 -0300

    Simplified types

--- src/store.ts
+++ src/store.ts
@@ -2,30 +2,26 @@ import { createEffect, createSignal, type Accessor, createMemo } from 'solid-js'
 
 const createSvelteSignal = <T>(value: T) => {
 	const [signal, setSignal] = createSignal<T>(value)
-  Object.assign(signal, {
-		set: (newValue: Exclude<T, Function> | ((prev: T) => T)) => setSignal(newValue),
-		subscribe: (fn: (value: T) => () => void) => {
-			createEffect(() => fn(signal()))
-			return () => {}
-		},
-	})
-	return [signal as Accessor<T> & {
-		set: (newValue: Exclude<T, Function> | ((prev: T) => T)) => void
-		subscribe: (fn: (value: T) => () => void) => void
-	}, setSignal] as const
+	return [
+		Object.assign(signal, {
+			set: (newValue: Exclude<T, Function> | ((prev: T) => T)) => setSignal(newValue),
+			subscribe: (fn: (value: T) => () => void) => {
+				createEffect(() => fn(signal()))
+				return () => {}
+			},
+		}),
+		setSignal,
+	] as const
 }
 
 const createSvelteMemo = <T>(fn: () => T) => {
 	const signal = createMemo(fn)
-	Object.assign(signal, {
+	return Object.assign(signal, {
 		subscribe: (fn: (value: T) => () => void) => {
 			createEffect(() => fn(signal()))
 			return () => {}
 		},
 	})
-	return signal as Accessor<T> & {
-		subscribe: (fn: (value: T) => () => void) => void
-	}
 }
 
 export const [count, setCount] = createSvelteSignal(0)
